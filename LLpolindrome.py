e9e00e73bb78fabaf2e3c5c86f71b9704785fa83
root <lseirina87@gmail.com>
2023-02-18 21:31:36 +0200

    add leetcode  LLpolindrome

---
+++ LLpolindrome.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        temp = head
+        stack = []
+        while temp:
+            stack.append(temp.val)
+            temp = temp.next
+        
+        temp = head
+        while temp:
+            if temp.val != stack.pop():
+                return False
+            temp = temp.next
+        return True
