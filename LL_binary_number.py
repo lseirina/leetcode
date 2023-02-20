3dc4549769a4a920b5aebc49862273483438beb5
root <lseirina87@gmail.com>
2023-02-20 21:46:52 +0200

    add leetcode

---
+++ LL_binary_number.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def getDecimalValue(self, head):
+        decimal_val = 0
+        temp = head
+        while temp:
+            decimal_val = decimal_val*2 + temp.val
+            temp = temp.next
+
+        return decimal_val
+            
