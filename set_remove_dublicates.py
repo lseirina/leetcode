c7b8a194a0787db8a44476055d9114d05d6a89c8
root <lseirina87@gmail.com>
2023-03-24 11:58:07 +0300

    add leetcode set

---
+++ set_remove_dublicates.py
@@ -0,0 +1,7 @@
+def remove_duplicates(my_list):
+    new_list = list(set(my_list))
+    return new_list
+
+my_list = [1, 2, 3, 4, 1, 2, 5, 6, 7, 3, 4, 8, 9, 5]
+new_list = remove_duplicates(my_list)
+print(new_list)
