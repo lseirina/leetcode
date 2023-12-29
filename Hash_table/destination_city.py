6ec7a02f9b725a56a93f5a30354954c7ce7b81d8
root <lseirina87@gmail.com>
2023-12-29 11:09:33 +0300

    Add hash table

---
+++ Hash_table/destination_city.py
@@ -0,0 +1,16 @@
+"""
+Input: paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+Output: "Sao Paulo"
+Explanation: Starting at "London" city you will reach "Sao Paulo" city which is the destination city. Your trip consist of: "London" -> "New York" -> "Lima" -> "Sao Paulo".
+Example 2:
+"""
+def destCity(paths):
+    cities = set()
+    for path in paths:
+        cities.add(path[0])
+
+    for path in paths:
+        if path[1] not in cities:
+            return path[1]
+
+print(destCity([["B","C"],["D","B"],["C","A"]]))
