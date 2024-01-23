d50413d31b60a79d6aa1e60fefcafd9710fcdfa4
root <lseirina87@gmail.com>
2024-01-23 22:49:32 +0300

    Added 2d hash

--- Hash_table/HT_2D_array.py
+++ Hash_table/HT_2D_array.py
@@ -13,11 +13,14 @@ It can be shown that we cannot have less than 3 rows in a valid array.
 from collections import Counter
 def find_matrix(nums):
     count = Counter(nums)
-    result = []
+    result = {}
+
     k = max(count.values())
-    A = list(count.elements()) #The elements() method is a part of the Counter class in Python. It returns an iterator over elements repeating each element as many times as its count.
-    #return [A[i::k] for i in range(k)] #The slicing syntax A[i::k] means to start from index i and to take every kth element from that point onwards.
-    return A
+    A = list(count.elements()) #The elements() method is a part of the Counter class in Python. It returns an iterator over
+                               #elements repeating each element as many times as its count.
+    return [A[i::k] for i in range(k)] #The slicing syntax A[i::k] means to start from index i and to take every kth
+    # element from that point onwards.
+    #return A
 
 print (find_matrix([1,3,4,1,2,3,1]))
 
