10e17739f00dfd5292b1b3bf4bf0453b364e5687
root <lseirina87@gmail.com>
2024-01-26 13:44:17 +0300

    Added sql

--- Hash_table/HT_smaller_numbers.py
+++ Hash_table/HT_smaller_numbers.py
@@ -10,20 +10,47 @@ For nums[3]=2 there exist one smaller number than it (1).
 For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
 """
 def smaller_numbers_then_current(nums):
+#def smallerNumbersThanCurrent(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        my_dict = {}
+        result = []
+        new_nums = sorted(nums)
+        for i, num in enumerate(new_nums):
+            if num not in my_dict:
+                my_dict[num] = i
+
+        for num in nums:
+            result.append(my_dict[num])
+
+        return result
 
 
-    temp = sorted(nums)
-    mapping = {}
-    result = []
 
 
-    for i in range(len(temp)):
-        if temp[i] not in mapping:
-            mapping[temp[i]] = i
-    for num in nums:
-        result.append(mapping[num])
 
-    return result
+
+
+
+
+
+
+
+
+#     temp = sorted(nums)
+#     mapping = {}
+#     result = []
+
+
+#     for i in range(len(temp)):
+#         if temp[i] not in mapping:
+#             mapping[temp[i]] = i
+#     for num in nums:
+#         result.append(mapping[num])
+
+#     return result
 
 
 
