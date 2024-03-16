a63b4abc4db6375c25e78c52006adf359449a685
root <lseirina87@gmail.com>
2024-03-16 22:23:59 +0300

    Added hash table

--- Hash_table/HT_jewels_stones.py
+++ Hash_table/HT_jewels_stones.py
@@ -2,38 +2,108 @@
 Input: jewels = "aA", stones = "aAAbbbb"
 Output: 3
 """
-#import collections
-def convert(func):
+def decorator(func):
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
         result += 1
         return result
     return wrapper
 
-@convert
-def num_jewels_in_stone(jewels, stones):
-    set_jewels = set(jewels)
+@decorator
+def count_jewels(jewels: str, stones: str):
     count = 0
+    jewels = set(jewels)
 
     for stone in stones:
-        if stone in set_jewels:
+        if stone in jewels:
             count += 1
 
     return count
 
+print(count_jewels('aA', 'AAAAaaaaaAaAaA'))
 
 
 def test():
-    assert num_jewels_in_stone("aA", "aAAbbbb") == 4
-    assert num_jewels_in_stone("B", "aAAbbbb") == 1
-    print('Tests passed')
-
+    assert count_jewels('', 'AAAAaaaaaAaAaA') == 1
+    print('Test passed')
 test()
-"""
-The list comprehension
-    return sum(stone in set_jewels for stone in stones)
 
-"""
 
-print(num_jewels_in_stone("aA","aAAbbbb"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#import collections
+# def convert(func):
+#     def wrapper(*args, **kwargs):
+#         result = func(*args, **kwargs)
+#         result += 1
+#         return result
+#     return wrapper
+
+# @convert
+# def num_jewels_in_stone(jewels, stones):
+#     set_jewels = set(jewels)
+#     count = 0
+
+#     for stone in stones:
+#         if stone in set_jewels:
+#             count += 1
+
+#     return count
+
+
+
+# def test():
+#     assert num_jewels_in_stone("aA", "aAAbbbb") == 4
+#     assert num_jewels_in_stone("B", "aAAbbbb") == 1
+#     print('Tests passed')
+
+# test()
+# """
+# The list comprehension
+#     return sum(stone in set_jewels for stone in stones)
+
+# """
+
+# print(num_jewels_in_stone("aA","aAAbbbb"))
 
