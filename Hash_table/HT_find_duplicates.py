33f1d88eb9169b86c49f446df84f410c1ca1dc30
root <lseirina87@gmail.com>
2024-03-18 13:24:35 +0300

    Added hash table

--- Hash_table/HT_find_duplicates.py
+++ Hash_table/HT_find_duplicates.py
@@ -1,30 +1,61 @@
 
-# Input: nums = [4, 3, 2, 7, 8, 2, 3, 1]
+# Input: nums = [4, 2, 7, 8, 3, 1]
 # Output: [2, 3]
 
+from collections import Counter
 
+def find_duplicate(nums):
+    c_nums = Counter(nums)
+    res = []
+    for num, i in dict(c_nums).items():
+        if i != 1:
+            res.append(num)
 
-def find_duplicates(nums):
-    num_counts = {}
-
-    for num in nums:
-        num_counts[num] = num_counts.get(num, 0) + 1
-
-    duplicates = [num for num, count in num_counts.items() if count > 1]   #list comprehension
-    # duplicates = []
-    # for num, count in num_counts.items():
-    #     if count > 1:
-    #         duplicates.append(num)
-    return duplicates
-
-print(find_duplicates([1,2,2,1]))
+    return res
 
 def test():
-    assert find_duplicates([1,2,2,3]) == [2]
-    assert find_duplicates([1,2,3]) == []
-    assert find_duplicates([]) == []
-    assert find_duplicates([1,1,1,1,1]) == [1]
+    assert find_duplicate([1, 2, 4, 5]) == []
+    assert find_duplicate([1, 1, 1, 1, 1]) == [1]
+    assert find_duplicate([1, -1]) == []
+    assert find_duplicate([2, 3, 2, 4, 5, 5]) == [2, 5]
 
     print('Tests passed')
 
 test()
+print(find_duplicate([4, 3, 2, 7, 8, 3, 2, 1]))
+
+
+
+
+
+
+
+
+
+
+
+
+# def find_duplicates(nums):
+#     num_counts = {}
+
+#     for num in nums:
+#         num_counts[num] = num_counts.get(num, 0) + 1
+
+#     duplicates = [num for num, count in num_counts.items() if count > 1]   #list comprehension
+#     # duplicates = []
+#     # for num, count in num_counts.items():
+#     #     if count > 1:
+#     #         duplicates.append(num)
+#     return duplicates
+
+# print(find_duplicates([1,2,2,1]))
+
+# def test():
+#     assert find_duplicates([1,2,2,3]) == [2]
+#     assert find_duplicates([1,2,3]) == []
+#     assert find_duplicates([]) == []
+#     assert find_duplicates([1,1,1,1,1]) == [1]
+
+#     print('Tests passed')
+
+# test()
---
+++ Hash_table/groupsizes.py
@@ -0,0 +1,31 @@
+"""
+Input: groupSizes = [3,3,3,3,3,1,3]
+Output: [[5],[0,1,2],[3,4,6]]
+Explanation:
+The first group is [5]. The size is 1, and groupSizes[5] = 1.
+The second group is [0,1,2]. The size is 3, and groupSizes[0] = groupSizes[1] = groupSizes[2] = 3.
+The third group is [3,4,6]. The size is 3, and groupSizes[3] = groupSizes[4] = groupSizes[6] = 3.
+Other possible solutions are [[2,1,6],[5],[0,4,3]] and [[5],[0,6,2],[4,3,1]].
+"""
+def  group_people(groupsizes):
+    i = 0
+    my_dict = {}
+    result = []
+
+    for num in groupsizes:
+        if num in my_dict:
+            my_dict[num].append(i)
+        else:
+            my_dict[num] = [i]
+        i += 1
+        if len(my_dict[num]) == num:
+            result.append(my_dict[num])
+            my_dict[num] = []
+
+    return result
+
+
+
+
+
+print(group_people([3,3,3,3,3,1,3]))
