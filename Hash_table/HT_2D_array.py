0e34f91ba900621aa3f4a59df269e49c790cc531
root <lseirina87@gmail.com>
2024-05-20 22:01:17 +0300

    Added leetcode hashtable 2d

--- Hash_table/HT_2D_array.py
+++ Hash_table/HT_2D_array.py
@@ -9,18 +9,40 @@ All elements of nums were used, and each row of the 2D array contains distinct i
 It can be shown that we cannot have less than 3 rows in a valid array.
 
 """
-
 from collections import Counter
-def find_matrix(nums):
+
+
+def create_2d_array(nums):
     count = Counter(nums)
-    result = {}
 
-    k = max(count.values())
-    A = list(count.elements()) #The elements() method is a part of the Counter class in Python. It returns an iterator over
-                               #elements repeating each element as many times as its count.
-    return [A[i::k] for i in range(k)] #The slicing syntax A[i::k] means to start from index i and to take every kth
-    # element from that point onwards.
-    #return A
+    ans = []
+    while count:
+        temp = []
+        res = []
+        for k, v in count.items():
+            temp.append(k)
+            v -= 1
+            if v == 0:
+                res.append(k)
+            count[k] = v
+        ans.append(temp)
+        for num in res:
+            del count[num]
 
-print (find_matrix([1,3,4,1,2,3,1]))
+    return ans
 
+
+print (create_2d_array([1, 3, 4, 1, 2, 3, 1]))
+
+
+# from collections import Counter
+# def find_matrix(nums):
+#     count = Counter(nums)
+#     result = {}
+
+#     k = max(count.values())
+#     A = list(count.elements()) #The elements() method is a part of the Counter class in Python. It returns an iterator over
+#                                #elements repeating each element as many times as its count.
+#     #return [A[i::k] for i in range(k)] #The slicing syntax A[i::k] means to start from index i and to take every kth
+#     # element from that point onwards.
+#     return A
