f85720fdb3e659ee7631a8bf07cbae7a7eebdba7
root <lseirina87@gmail.com>
2024-05-24 21:42:08 +0300

    Added leetcode hashtabl

--- Hash_table/HT_two_sum.py
+++ Hash_table/HT_two_sum.py
@@ -7,6 +7,13 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]
 
 
 
+
+
+
+
+
+
+
 def two_sum(nums, target):
     num_map = {}
 
@@ -17,32 +24,3 @@ def two_sum(nums, target):
             num_map[num] = i
 
 print(two_sum([2,7,11,15], 9))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Hash_table/largest_substring.py
+++ Hash_table/largest_substring.py
@@ -1,13 +1,64 @@
+"""
+return the length of the longest substring between two equal characters, excluding the two characters.
+Example 2:
+
+Input: s = "abca"
+Output: 2
+Explanation: The optimal substring here is "bc".
+Example 3:
+
+Input: s = "cbzxy"
+Output: -1
+Explanation: There are no characters that appear twice in s.
+"""
+def find_longest_substring(s):
+    first_occurance = {}
+    max_length = -1
+    for i, char in enumerate(s):
+        if char in first_occurance:
+            length = i - first_occurance[char] - 1
+            max_length = max(max_length, length)
+        else:
+            first_occurance[char] = i
+    return max_length
+print(find_longest_substring("abghjca"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def max_length(s):
     my_dict = {}
     a = -1
     for i in range(len(s)):
         if s[i] in my_dict:
-            a = max(a, ( i - my_dict[s[i]] - 1))
+            a = max(a, (i - my_dict[s[i]] - 1))
         else:
             my_dict[s[i]] = i
 
     return a
 
 
-print(max_length("mgntdygtxrvxjnwksqhxuxtrv"))
+print(max_length("mgntdygtxrvxjndwksqhxuxtrvm"))
