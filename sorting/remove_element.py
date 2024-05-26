01d89a1f2ab86bc6ba59f4f979cb2849a9768745
root <lseirina87@gmail.com>
2024-05-26 14:40:05 +0300

    Added sorting

--- sorting/remove_element.py
+++ sorting/remove_element.py
@@ -1,11 +1,6 @@
-def my_decorator(func):
-    def wrapper(*args, **kwargs):
-        result = func(*args, **kwargs)
-        result += 1
-        return result
-    return wrapper
+# remove_element([1, 1, 3, 1, 4, 1, 1, 4], 1))
+# output: 3 - 3 numbers(3, 4, 4) are not '1'
 
-@my_decorator
 def remove_element(nums, val):
     i = 0
     while i < len(nums):
@@ -14,20 +9,9 @@ def remove_element(nums, val):
         else:
             i += 1
 
-    return len(nums)
+    return nums
 
-print(remove_element([1, 1, 1, 4, 1], 1))
-
-def test():
-    assert remove_element([], 2) == 1
-
-    print('Tests passed')
-
-
-
-
-
-test()
+print(remove_element([1, 1, 3, 1, 4, 1, 1], 1))
 
 
 
@@ -39,10 +23,17 @@ test()
 
 
 
+# def remove_element(nums, val):
+#     i = 0
+#     while i < len(nums):
+#         if nums[i] == val:
+#             nums.pop(i)
+#         else:
+#             i += 1
 
+#     return len(nums)
 
-
-
+# print(remove_element([1, 1, 3, 1, 4, 1, 1, 4], 1))
 
 
 # def remove_element(nums, val):
