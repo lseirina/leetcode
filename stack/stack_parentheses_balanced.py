ece76e02918acdd7e202b5869926f3660b999b3c
root <lseirina87@gmail.com>
2024-05-27 10:57:48 +0300

    Added leetcode stack

--- stack/stack_parentheses_balanced.py
+++ stack/stack_parentheses_balanced.py
@@ -1,39 +1,58 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def balanced_parentheses(s):
-
-
-#     res = 0
-#     for c in s:
-#         if c == "(":
-#             res += 1
-
-#         if c == ")":
-#             res -= 1
-#     if res == 0:
-#         return True
-
-#     else:
-#         return False
-
 # print(balanced_parentheses("((()))"))
+
+def balanced_parentheses(chars):
+
+    count = 0
+    for c in chars:
+        if c == '(':
+            count += 1
+        if c == ')':
+            count -= 1
+
+    if count == 0:
+        return True
+    else:
+        return False
+
+
+
+print(balanced_parentheses("((())))"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def balanced_parentheses(s):
+
+
+    res = 0
+    for c in s:
+        if c == "(":
+            res += 1
+
+        if c == ")":
+            res -= 1
+    if res == 0:
+        return True
+
+    else:
+        return False
+
+print(balanced_parentheses("((()))"))
