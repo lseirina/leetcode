019a4055019c02e4a6cc45172d9d4de4ecb2724b
root <lseirina87@gmail.com>
2024-05-27 21:45:57 +0300

    Added leetcode

--- stack/stack_rm_out_parentheses.py
+++ stack/stack_rm_out_parentheses.py
@@ -2,6 +2,28 @@
 Input: s = "(()())(())"
 Output: "()()()"
 """
+def remove_out_parentheses(s):
+    res = []
+    count = 0
+    for c in s:
+        if c == '(' and count > 0:
+            res.append(c)
+        if c == ')' and count > 1:
+            res.append(c)
+
+        if c == '(':
+            count += 1
+        else:
+            count -= 1
+
+    return ''.join(res)
+
+print(remove_out_parentheses("(()())(())"))
+
+
+
+
+print(remove_out_parentheses("(()())(())(()(()))"))
 
 
 
@@ -25,21 +47,20 @@ Output: "()()()"
 
 
 
+def removeOuterParentheses(S):
+        res = []
+        opened = 0
+        for c in S:
+            if c == '(' and opened > 0:
+                res.append(c)
+            if c == ')' and opened > 1:
+                res.append(c)
 
-# def removeOuterParentheses(S):
-#         res = []
-#         opened = 0
-#         for c in S:
-#             if c == '(' and opened > 0:
-#                 res.append(c)
-#             if c == ')' and opened > 1:
-#                 res.append(c)
-
-#             if c == '(':
-#                 opened += 1
-#             else:
-#                 opened -= 1
-#         return "".join(res)
+            if c == '(':
+                opened += 1
+            else:
+                opened -= 1
+        return "".join(res)
 
 
-# print(removeOuterParentheses("((())())(())"))
+print(removeOuterParentheses("((())())(())"))
