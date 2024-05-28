c2d8629b565974cead6fb9185bb888a8db020375
root <lseirina87@gmail.com>
2024-05-28 14:35:33 +0300

    Added leetcode

--- String/acronym.py
+++ String/acronym.py
@@ -1,3 +1,53 @@
+# print(isAcronym(["alice","bob","charlie"], "abc")) True
+
+def my_decorator(func):
+    def wrapper(*args, **kwargs):
+        resultat = func(*args, **kwargs)
+        resultat = False
+        return resultat
+    return wrapper
+
+
+def confirm_acronym(words, acronym):
+    res = []
+    for word in words:
+        res.append(word[0])
+    if ''.join(res) == acronym:
+        return True
+    return False
+
+
+print(confirm_acronym(["alice","bob","charlie"], "abc"))
+
+
+def test_confirm_acronym():
+    assert confirm_acronym(["alice","bob","charlie"], "abc") == True
+    print('Tests passed')
+
+test_confirm_acronym()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def isAcronym(words, s):
         res = []
         for word in words:
