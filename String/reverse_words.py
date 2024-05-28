ab427f0df28f6653e10b319c6307512d2da05bb6
root <lseirina87@gmail.com>
2024-05-28 21:10:09 +0300

    Added leetcode str

--- String/reverse_words.py
+++ String/reverse_words.py
@@ -1,3 +1,39 @@
+# print(reverseWords("Let's take LeetCode contest"))
+# output: s'teL ekat edoCteeL tsetnoc
+
+
+def reverse_words(sentence):
+    sentence = sentence.split(' ')
+    i = 0
+    for i in range(len(sentence)):
+        sentence[i] = sentence[i][::-1]
+        i += 1
+
+
+    return ' '.join(sentence)
+
+print(reverse_words("Let's take LeetCode contest"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def reverseWords(s):
         s = s.split()
         res = []
--- String/sorting_sentence.py
+++ String/sorting_sentence.py
@@ -1,15 +1,28 @@
+
 # Input: s = "is2 sentence4 This1 a3"
 # Output: "This is a sentence"
 
-def sortSentence(s):
-    s = s.split()
-    #res = [''] * len(s)
-    res = []
-    for c in s:
-        num = int(c[-1])
-        res[num-1] = c[:-1]
-
+def sort_sentence(words: str):
+    new_words = words.split()
+    res = [''] * len(new_words)
+    for word in new_words:
+        num = int(word[-1])
+        res[num-1] = word[:-1]
     return ' '.join(res)
 
-print(sortSentence("is2 sentence4 This1 a3"))
 
+print(sort_sentence("is2 sentence4 This1 a3"))
+
+
+
+# def sortSentence(s):
+#     s = s.split()
+#     res = [''] * len(s)
+#     # res = []
+#     for c in s:
+#         num = int(c[-1])
+#         res[num-1] = c[:-1]
+
+#     return ' '.join(res)
+
+# print(sortSentence("is2 sentence4 This1 a3"))
