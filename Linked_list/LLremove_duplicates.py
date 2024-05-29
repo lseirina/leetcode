0000751bd618cf7d70aff3bf8e0f32a43950e211
root <lseirina87@gmail.com>
2024-05-29 20:46:51 +0300

    Added leetcode LL

--- Linked_list/LLremove_duplicates.py
+++ Linked_list/LLremove_duplicates.py
@@ -3,14 +3,56 @@ class ListNode:
         self.val = val
         self.next = next
 
+
+
+def remove_dublicates(self, head):
+    temp = head
+    while temp and temp.next:
+        if temp.val == temp.next.val:
+            temp = temp.next.next
+        else:
+            temp = temp.next
+
+    return head
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def deleteDuplicates(head: ListNode) -> ListNode:
     curr = head
-    
+
     while curr and curr.next:
         if curr.val == curr.next.val:
             curr.next = curr.next.next
         else:
             curr = curr.next
-    
-    return head
 
+    return head
