3f26d59f0d6bc491b5994845c3534ea2e3e3a222
root <lseirina87@gmail.com>
2024-05-29 14:15:38 +0300

    Added leetcode str

--- String/ST_bakanced_str_split.py
+++ String/ST_bakanced_str_split.py
@@ -5,6 +5,22 @@ Explanation: s can be split into "RL", "RRLL", "RL", "RL",
 each substring contains same number of 'L' and 'R'.
 """
 
+# def get_balanced_str(s):
+#     count = 0
+#     res = 0
+#     for c in s:
+#         if c == 'R':
+#             count += 1
+#         else:
+#             count -= 1
+#         if c == 0:
+#             res += 1
+
+#     return res
+
+
+# print(get_balanced_str("RLRRLLRLRL"))
+
 def split_balanced_string(s):
     count = 0
     res = 0
@@ -19,11 +35,11 @@ def split_balanced_string(s):
     return res
 
 
-print(split_balanced_string("RLRRRLLRLL"))
+print(split_balanced_string("RLRRLLRLRL"))
 
-def test():
-    assert split_balanced_string("RLRRRLLRLL") == 2
-    assert split_balanced_string("RLRRR") == 2
-    print('tests passed')
+# def test():
+#     assert split_balanced_string("RLRRRLLRLL") == 2
+#     assert split_balanced_string("RLRRR") == 2
+#     print('tests passed')
 
-test()
+# test()
--- String/sorting_sentence.py
+++ String/sorting_sentence.py
@@ -2,27 +2,15 @@
 # Input: s = "is2 sentence4 This1 a3"
 # Output: "This is a sentence"
 
-def sort_sentence(words: str):
-    new_words = words.split()
-    res = [''] * len(new_words)
-    for word in new_words:
-        num = int(word[-1])
-        res[num-1] = word[:-1]
+
+def sortSentence(s):
+    s = s.split()
+    res = [''] * len(s)
+    # res = []
+    for c in s:
+        num = int(c[-1])
+        res[num-1] = c[:-1]
+
     return ' '.join(res)
 
-
-print(sort_sentence("is2 sentence4 This1 a3"))
-
-
-
-# def sortSentence(s):
-#     s = s.split()
-#     res = [''] * len(s)
-#     # res = []
-#     for c in s:
-#         num = int(c[-1])
-#         res[num-1] = c[:-1]
-
-#     return ' '.join(res)
-
-# print(sortSentence("is2 sentence4 This1 a3"))
+print(sortSentence("is2 sentence4 This1 a3"))
