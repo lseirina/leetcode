90623d296046e6f8504363cfe5b6529fc439e92f
root <lseirina87@gmail.com>
2024-05-31 21:55:39 +0300

    Added leetcode

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -7,5 +7,13 @@
 # Output: ""
 # Explanation: There is no common prefix among the input strings.
 
+def find_longest_prefix(words: list[str]) -> str:
+    res = []
+    short_word = min(words, key=len)
+    for i in range(len(short_word)):
+        for word in words.remove(short_word):
+            if word[i] == short_word[i]:
+                res.append(short_word[:i+1])
+    return res
 
-
+print(find_longest_prefix(["flower", "flow", "flight"]))
--- string_reverse_words_in_string.py
+++ string_reverse_words_in_string.py
@@ -1,16 +1,61 @@
 # Input: s = "Let's take LeetCode contest"
 # Output: "s'teL ekat edoCteeL tsetnoc"
+def my_decorator(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        result = result.upper()
+        return result
+    return wrapper
 
-def reverse_words(words):
-    word = words.split(" ")
-    
-    for i in range(len(word)):
-        word[i] = word[i][::-1]
-    
-    return " ".join(word)
-        
 
-    
-        
+@my_decorator
+def reverse_words(s):
+    res = s.split()
+    for i in range(len(res)):
+        res[i] = res[i][::-1]
+    return ' '.join(res)
+
 
 print(reverse_words("Let's take LeetCode contest"))
+
+
+def test_reverse_words():
+    assert reverse_words("Let's take LeetCode contest") == "S'TEL EKAT EDOCTEEL TSETNOC"
+    assert reverse_words('') == ''
+
+    print('Tests passed')
+
+
+
+test_reverse_words()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def reverse_words(words):
+#     word = words.split(" ")
+
+#     for i in range(len(word)):
+#         word[i] = word[i][::-1]
+
+#     return " ".join(word)
+
+
+
+
+
+# print(reverse_words("Let's take LeetCode contest"))
