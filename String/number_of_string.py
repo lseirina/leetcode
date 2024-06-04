b91fe2df500fa03a2e090a062b8f8c63a822460d
root <lseirina87@gmail.com>
2024-06-04 20:16:15 +0300

    Added leetcode str

---
+++ String/number_of_string.py
@@ -0,0 +1,16 @@
+"""
+Input: patterns = ["a","abc","bc","d"], word = "abc"
+Output: 3
+Explanation:
+- "a" appears as a substring in "abc".
+- "abc" appears as a substring in "abc".
+- "bc" appears as a substring in "abc".
+- "d" does not appear as a substring in "abc".
+3 of the strings in patterns appear as a substring in word.
+"""
+def count_number_of_strings(patterns, word)
+    count = 0
+    for pattern in patterns:
+        if pattern in word:
+            count += 1
+    return count
