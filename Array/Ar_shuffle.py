52251b0823584cdd6e34d60dcd207714af486f44
root <lseirina87@gmail.com>
2024-07-13 21:39:14 +0300

    Added max_wealth

--- Array/Ar_shuffle.py
+++ Array/Ar_shuffle.py
@@ -35,18 +35,3 @@ print(shuffle_array([2,5,1,3,4,7], 3))
 
 
 
-
-
-
-
-
-
-# def shuffle_array(nums, n):
-#     result = []
-#     for i in range(n):
-#        result.append(nums[i])
-#        result.append(nums[i + n])
-
-#     return result
-
-# print(shuffle_array([2,5,1,3,4,7], 3))
--- Array/max_wealth.py
+++ Array/max_wealth.py
@@ -1,5 +1,5 @@
 """
-Input: accounts = [[1,2,3],[3,2,1]]
+Input: accounts = [[1,2,3],[3,6,1]]
 Output: 6
 Explanation:
 1st customer has wealth = 1 + 2 + 3 = 6
@@ -7,14 +7,28 @@ Explanation:
 Both customers are considered the richest with a wealth of 6 each, so return 6.
 """
 
-
-
-
-
-
-
 def find_max_wealth(accounts):
-    """Find the maximim wealth."""
-    return max([sum(account) for account in accounts])
+   return max([sum(account) for account in accounts])
 
-print(find_max_wealth([[1,2,3],[3,2,1]]))
+print(find_max_wealth([[1,2,3],[3,7,1]]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def find_max_wealth(accounts):
+#     """Find the maximim wealth."""
+#     return max([sum(account) for account in accounts])
+
+# print(find_max_wealth([[1,2,3],[3,2,1]]))
