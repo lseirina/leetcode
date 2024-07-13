b9c25bf48e5ee2dcab372f9d887f3c8782c6f572
root <lseirina87@gmail.com>
2024-07-13 16:58:21 +0300

    Added left right difference

--- Array/Ar_left_right_difference.py
+++ Array/Ar_left_right_difference.py
@@ -5,53 +5,66 @@ Explanation: The array leftSum is [0,10,14,22] and the array rightSum is [15,11,
 The array answer is [|0 - 15|,|10 - 11|,|14 - 3|,|22 - 0|] = [15,1,11,22].
 """
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def find_difference(nums):
-
     n = len(nums)
     left_sum = [0] * n
     right_sum = [0] * n
 
     for i in range(1, n):
         left_sum[i] = left_sum[i-1] + nums[i-1]
-
     for i in range(n-2, -1, -1):
         right_sum[i] = right_sum[i+1] + nums[i+1]
 
-
-    answer = [abs(left_sum[i] - right_sum[i]) for i in range(n)]
-    return answer
+    return [abs(left_sum[i] - right_sum[i]) for i in range(n)]
 
 print(find_difference([10,4,8,3]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def find_difference(nums):
+
+#     n = len(nums)
+#     left_sum = [0] * n
+#     right_sum = [0] * n
+
+#     for i in range(1, n):
+#         left_sum[i] = left_sum[i-1] + nums[i-1]
+
+#     for i in range(n-2, -1, -1):
+#         right_sum[i] = right_sum[i+1] + nums[i+1]
+
+
+#     answer = [abs(left_sum[i] - right_sum[i]) for i in range(n)]
+#     return answer
+
+# print(find_difference([10,4,8,3]))
