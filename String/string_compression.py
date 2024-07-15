bd4d530d56c7964849505bbef129935e3c142dde
root <lseirina87@gmail.com>
2024-07-15 21:18:23 +0300

    Added string compession 3

--- String/string_compression.py
+++ String/string_compression.py
@@ -1,21 +1,47 @@
+"""
+input: word = "aaaaaaaaaaaaaabb"
+Output: "9a5a2b
+"""
+
 def find_compress_str(word):
+    res = []
+    count = 0
 
-    comp = ''
-    i = 0
-
-    while i < len(word):
-        char = word[i]
-        count = 0
-
-        while i < len(word) and word[i] == char and count < 9:
+    for i in range(len(word)-1):
+        if word[i] == word[i+1] and count < 9:
             count += 1
-            i += 1
 
-        comp += str(count) + char
+        else:
+            res.append(str(count))
+            res.append(word[i])
+            count = 1
 
-    return comp
+    res.append(str(count))
+    res.append(word[i])
+
+    return ''.join(res)
+
 
 print(find_compress_str("aaaaaaaaaaaaaabb"))
+
+
+
+#     comp = ''
+#     i = 0
+
+#     while i < len(word):
+#         char = word[i]
+#         count = 0
+
+#         while i < len(word) and word[i] == char and count < 9:
+#             count += 1
+#             i += 1
+
+#         comp += str(count) + char
+
+#     return comp
+
+# print(find_compress_str("aaaaaaaaaaaaaabb"))
 
 
 
