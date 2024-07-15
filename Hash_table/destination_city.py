ba9997ed204812d97e6432e0f8451c28d721be28
root <lseirina87@gmail.com>
2024-07-15 21:48:11 +0300

    Added destination city

--- Hash_table/destination_city.py
+++ Hash_table/destination_city.py
@@ -7,16 +7,50 @@ print(destCity([["C","A"],["B","C"],["D","B"]]))
 """
 
 
+def find_destination(cities):
+    my_dict = {}
+    for city in cities:
+        my_dict[city[0]] = city
+
+    for city in cities:
+        if city[1] not in my_dict.keys():
+            return city[1]
+
+print(find_destination([["C","A"],["B","C"],["D","B"]]))
 
 
 
-def destCity(paths):
-    cities = set()
-    for path in paths:
-        cities.add(path[0])
 
-    for path in paths:
-        if path[1] not in cities:
-            return path[1]
 
-print(destCity([["B","C"],["D","B"],["C","A"]]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def destCity(paths):
+#     cities = set()
+#     for path in paths:
+#         cities.add(path[0])
+
+#     for path in paths:
+#         if path[1] not in cities:
+#             return path[1]
+
+# print(destCity([["B","C"],["D","B"],["C","A"]]))
