1a19824c75747e65ff82bc03a4d4d2fbe6119c46
root <lseirina87@gmail.com>
2024-07-15 20:56:40 +0300

    Added string compression modified

--- String/string_compression.py
+++ String/string_compression.py
@@ -1,22 +1,44 @@
-"""
-Input: word = "abcde"
-Output: "1a1b1c1d1e"
+def find_compress_str(word):
 
-"""
-def compress_str(word):
-    count = 1
-    compress_str = ''
-    new_char = word[0]
-    for i in range(1, len(word)):
-        if word[i] == new_char and count < 9:
+    comp = ''
+    i = 0
+
+    while i < len(word):
+        char = word[i]
+        count = 0
+
+        while i < len(word) and word[i] == char and count < 9:
             count += 1
-        else:
-            compress_str += str(count) + new_char
-            new_char = word[i]
-            count = 1
+            i += 1
 
-    compress_str += str(count) + new_char
+        comp += str(count) + char
 
-    return compress_str
+    return comp
 
-print(compress_str("aaaaaaaaaaaaaabb"))
+print(find_compress_str("aaaaaaaaaaaaaabb"))
+
+
+
+
+
+
+
+
+
+
+#     count = 1
+#     compress_str = ''
+#     new_char = word[0]
+#     for i in range(1, len(word)):
+#         if word[i] == new_char and count < 9:
+#             count += 1
+#         else:
+#             compress_str += str(count) + new_char
+#             new_char = word[i]
+#             count = 1
+
+#     compress_str += str(count) + new_char
+
+#     return compress_str
+
+# print(find_compress_str("aaaaaaaaaaaaaabb"))
