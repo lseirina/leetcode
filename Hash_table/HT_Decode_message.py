939ab9203e3c1c1cd4e95bd30fab69453a61e4ee
root <lseirina87@gmail.com>
2024-07-16 22:03:25 +0300

    Added decode message

--- Hash_table/HT_Decode_message.py
+++ Hash_table/HT_Decode_message.py
@@ -4,46 +4,91 @@ Output: "this is a secret"
 Explanation: The diagram above shows the substitution table.
 It is obtained by taking the first appearance of each letter in "the quick brown fox jumps over the lazy dog".
 """
+from collections import defaultdict
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def convert(func):
-    def wrapper(*args, **kwargs):
-        result = func(*args, **kwargs)
-        result = result.upper()
-        return result
-    return wrapper
-
-
-@convert
 def decode_message(key, message):
-    mapping = {" ": " "}
     letters = 'abcdefghijklmnopqrstuvwxyz'
     res = ''
-    count = 0
-    for char in key:
-        if char not in mapping:
-            mapping[char] = letters[count]
-            count += 1
-    for i in message:
-        res += mapping[i]
+    ans = ''
+    my_dict = {}
+    i = 0
+    for c in key.replace(' ', ''):
+        if c not in res:
+            res += c
 
-    return res
+    for c in res:
+        my_dict[c] = letters[i]
+        i += 1
+
+    for letter in message:
+        if letter == ' ':
+            ans += ' '
+        else:
+            ans += my_dict[letter]
 
 
+
+    return ans
 
 print(decode_message("the quick brown fox jumps over the lazy dog", "vkbs bs t suepuv"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def convert(func):
+#     def wrapper(*args, **kwargs):
+#         result = func(*args, **kwargs)
+#         result = result.upper()
+#         return result
+#     return wrapper
+
+
+# @convert
+# def decode_message(key, message):
+#     mapping = {" ": " "}
+#     letters = 'abcdefghijklmnopqrstuvwxyz'
+#     res = ''
+#     count = 0
+#     for char in key:
+#         if char not in mapping:
+#             mapping[char] = letters[count]
+#             count += 1
+#     for i in message:
+#         res += mapping[i]
+
+#     return res
+
+
+
+# print(decode_message("the quick brown fox jumps over the lazy dog", "vkbs bs t suepuv"))
--- Hash_table/HT_arithmatic_triplets.py
+++ Hash_table/HT_arithmatic_triplets.py
@@ -6,21 +6,6 @@ Explanation:
 (2, 4, 5) is an arithmetic triplet because both 10 - 7 == 3 and 7 - 4 == 3.
 """
 
-def find_arithmatic_triplets(nums, diff):
-    set_nums = set(nums)
-    count = 0
-    for num in set_nums:
-        if (num + diff) in set_nums and (num + 2*diff) in set_nums:
-            count += 1
-
-    return count
-
-
-print(find_arithmatic_triplets([0,1,4,6,7,10], 3))
-
-
-
-
 
 
 
