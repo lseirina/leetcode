d03551e64fac03c692b21bf67363279bcc423046
root <lseirina87@gmail.com>
2024-07-16 17:01:11 +0300

    Added h_t_k_difference

--- Hash_table/H_T_k_difference.py
+++ Hash_table/H_T_k_difference.py
@@ -6,58 +6,51 @@ Explanation: The pairs with an absolute difference of 2 are:
 - [3,2,1,5,4]
 - [3,2,1,5,4]
 
-"""
+ """
 from collections import Counter
 
-def find_pairs(nums, k):
-    dict_nums = Counter(nums)
-    count = 0
-    for num in nums:
-        if num - k in dict_nums:
-            count += dict_nums[num - k]
-
-    return count
-
-print(find_pairs([1,2,2,1], 1))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-from collections import Counter
-
-
-from collections import defaultdict, Counter
-
-
-def count_pairs(nums, k):
-    dict_nums = Counter(nums)
+def find_pairs_with_difference(nums, k):
+    my_dict = Counter(nums)
     count = 0
 
     for num in nums:
-        if num - k in dict_nums:
-            count += dict_nums[num - k]
+        if (num - k) in my_dict:
+            count += my_dict[num-k]
 
-    return count
+    return my_dict
 
 
-print(count_pairs([3,2,1,5,4], 2))
+
+print(find_pairs_with_difference([1,2,2,1,2], 1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from collections import Counter
+
+# def find_pairs(nums, k):
+#     dict_nums = Counter(nums)
+#     count = 0
+#     for num in nums:
+#         if num - k in dict_nums:
+#             count += dict_nums[num - k]
+
+#     return count
+
+# print(find_pairs([1,2,2,1], 1))
