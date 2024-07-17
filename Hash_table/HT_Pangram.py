b60e70ffac6813141a024d4d87b9810a6f9ef615
root <lseirina87@gmail.com>
2024-07-17 21:31:46 +0300

    Added pangram

--- Hash_table/HT_Pangram.py
+++ Hash_table/HT_Pangram.py
@@ -5,23 +5,37 @@ Output: true
 Explanation: sentence contains at least one of every letter of the English alphabet.
 """
 
-
-
-
-
-
-
-
-
-def check_pangram(chars):
-
-    return len(set(chars)) == 26
+def check_pangram(s):
+    return len(set(s)) == 26
 
 print(check_pangram("thequickbrownfoxjumpsoverthelazydog"))
 
-def test():
-    assert check_pangram('jkjfods438490328590') == False
-    assert check_pangram('') == False
-    print('Tests passed')
 
-test()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def check_pangram(chars):
+
+#     return len(set(chars)) == 26
+
+# print(check_pangram("thequickbrownfoxjumpsoverthelazydog"))
+
+# def test():
+#     assert check_pangram('jkjfods438490328590') == False
+#     assert check_pangram('') == False
+#     print('Tests passed')
+
+# test()
