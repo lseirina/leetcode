7ecbb5d7bfbd34e3881f649bc3551fad066a66ca
root <lseirina87@gmail.com>
2024-07-17 15:31:59 +0300

    Added good_pairs

--- Hash_table/HT_good_pairs.py
+++ Hash_table/HT_good_pairs.py
@@ -1,9 +1,49 @@
-import collections
+
 """
 Input: nums = [1,2,3,1,1,3]
 Output: 4
 Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-index.
 """
+from collections import Counter
+
+def find_good_pairs(nums):
+    my_dict = Counter(nums)
+    count = 0
+
+    for num in my_dict.values():
+        count += num * (num - 1) // 2
+
+    return count
+
+
+print(find_good_pairs([1,2,3,1,1,3]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # def find_good_pairs(nums):
@@ -25,13 +65,15 @@ Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-index.
 
 # print(find_good_pairs([1,1,1,1]))
 
-def find_good_pairs(nums):
-    freq = collections.Counter(nums)
-    count = 0
+# def find_good_pairs(nums):
+#     freq = collections.Counter(nums)
+#     count = 0
 
-    for num in freq.values():
-        count += num * (num - 1) // 2  # the double forward slashes (//) are used for integer division in Python.
+#     for num in freq.values():
+"""Формула для количества способов выбрать 2 элемента из num элементов (без учёта порядка) — это комбинаторная функция C(num, 2)
+, которая определяет сколько существует способов выбрать 2 элемента из этого множества"""
+#         count += num * (num - 1) // 2  # the double forward slashes (//) are used for integer division in Python.
 
-    return count
+#     return count
 
-print(find_good_pairs([1,1,1,1,3]))
+# print(find_good_pairs([1,1,1,1,3]))
