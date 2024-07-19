48bc5a4c38cf68985f7c76f52a37b14feb47095c
root <lseirina87@gmail.com>
2024-07-19 19:53:37 +0300

    Added cycle

--- Linked_list/lLL_cycle.py
+++ Linked_list/lLL_cycle.py
@@ -6,6 +6,33 @@ class ListNode(object):
 
 class Solution(object):
     def hasCycle(self, head):
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+
+        return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         fast = slow = head
         while fast and fast.next:
             slow = slow.next
