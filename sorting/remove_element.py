82ef66a625a48712ac667e3da680c1fda04c0b73
root <lseirina87@gmail.com>
2024-07-20 20:33:11 +0300

    Added remove_element

--- sorting/remove_element.py
+++ sorting/remove_element.py
@@ -1,17 +1,60 @@
 # remove_element([1, 1, 3, 1, 4, 1, 1, 4], 1))
 # output: 3 - 3 numbers(3, 4, 4) are not '1'
 
-def remove_element(nums, target):
-    i = 0
-    while i < len(nums):
-        if nums[i] == target:
-            nums.pop(i)
-        else:
-            i += 1
-    return nums
 
+def remove_element(nums, target):
+    count = 0
+    for num in nums:
+        if num != target:
+            count += 1
+
+    return count
 
 print(remove_element([1, 1, 3, 1, 4, 1, 1, 4], 1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def remove_element(nums, target):
+#     i = 0
+#     while i < len(nums):
+#         if nums[i] == target:
+#             nums.pop(i)
+#         else:
+#             i += 1
+#     return nums
+
+
+# print(remove_element([1, 1, 3, 1, 4, 1, 1, 4], 1))
 
 
 
