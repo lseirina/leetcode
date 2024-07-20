eace40f02ecddbcb8237dea3393028f40fc5d521
root <lseirina87@gmail.com>
2024-07-20 17:15:52 +0300

    Added find_sequence

--- set/set_consecutive_sequence.py
+++ set/set_consecutive_sequence.py
@@ -4,6 +4,36 @@ Output: 4
 Explanation: The longest consecutive sequence in the input array is [4, 3, 2, 1], and its length is 4.
 """
 
+def find_consecutive_sequence(nums):
+    longest_sequence = 0
+    for num in nums:
+        if num - 1 not in nums:
+            current_num = num
+            current_sequence = 1
+
+            while current_num + 1 in nums:
+                current_num += 1
+                current_sequence += 1
+
+            longest_sequence = max(longest_sequence, current_sequence)
+
+    return longest_sequence
+
+
+print(find_consecutive_sequence([100, 4, 200, 1, 3, 2]))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -25,4 +55,4 @@ def cc(nums):
 
     return current_sequence
 
-print(cc([0, 0, -1]))
+print(cc([100, 4, 200, 1, 3, 2]))
