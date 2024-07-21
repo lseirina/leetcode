9bb820bfcdb08061b95e6c87c08eee43a5f9c385
root <lseirina87@gmail.com>
2024-07-21 20:39:22 +0300

    Added rm_out_parentheses

--- stack/stack_rm_out_parentheses.py
+++ stack/stack_rm_out_parentheses.py
@@ -2,6 +2,8 @@
 Input: s = "(()())(())"
 Output: "()()()"
 """
+
+
 def remove_out_parentheses(s):
     res = []
     count = 0
@@ -18,12 +20,12 @@ def remove_out_parentheses(s):
 
     return ''.join(res)
 
+
 print(remove_out_parentheses("(()())(())"))
 
 
 
 
-print(remove_out_parentheses("(()())(())(()(()))"))
 
 
 
@@ -47,20 +49,35 @@ print(remove_out_parentheses("(()())(())(()(()))"))
 
 
 
-def removeOuterParentheses(S):
-        res = []
-        opened = 0
-        for c in S:
-            if c == '(' and opened > 0:
-                res.append(c)
-            if c == ')' and opened > 1:
-                res.append(c)
-
-            if c == '(':
-                opened += 1
-            else:
-                opened -= 1
-        return "".join(res)
 
 
-print(removeOuterParentheses("((())())(())"))
+
+
+
+
+
+
+
+
+
+
+# def remove_out_parentheses(s):
+#     res = []
+#     count = 0
+#     for c in s:
+#         if c == '(' and count > 0:
+#             res.append(c)
+#         if c == ')' and count > 1:
+#             res.append(c)
+
+#         if c == '(':
+#             count += 1
+#         else:
+#             count -= 1
+
+#     return ''.join(res)
+
+# print(remove_out_parentheses("(()())(())"))
+
+
+# print(remove_out_parentheses("(()())(())(()(()))"))
