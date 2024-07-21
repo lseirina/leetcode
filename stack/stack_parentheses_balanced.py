23c44e7c6654d841bdec579095a2bce77feccec9
root <lseirina87@gmail.com>
2024-07-21 17:43:53 +0300

    Added remove_duplicates-nabours

--- stack/stack_parentheses_balanced.py
+++ stack/stack_parentheses_balanced.py
@@ -1,56 +1,21 @@
 # print(balanced_parentheses("((()))"))
 
-def find_balanced_parentheses(s):
-    count = 0
-    for c in s:
-        if c == '(':
-            count += 1
-        else:
-            count -= 1
 
-    if count == 0:
+
+def balanced_parentheses(s):
+
+
+    res = 0
+    for c in s:
+        if c == "(":
+            res += 1
+
+        if c == ")":
+            res -= 1
+    if res == 0:
         return True
+
     else:
         return False
 
-print(find_balanced_parentheses("((()))"))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def balanced_parentheses(s):
-
-
-#     res = 0
-#     for c in s:
-#         if c == "(":
-#             res += 1
-
-#         if c == ")":
-#             res -= 1
-#     if res == 0:
-#         return True
-
-#     else:
-#         return False
-
-# print(balanced_parentheses("((()))"))
+print(balanced_parentheses("((()))"))
--- stack/stack_remove_dublicats_nabours.py
+++ stack/stack_remove_dublicats_nabours.py
@@ -5,9 +5,9 @@
 # and this is the only possible move.  The result of this move is that the string is "aaca",
 # of which only "aa" is possible, so the final string is "ca".
 
-def remove_dublicates(chars: str):
+def remove_duplicates_nabours(s):
     stack = []
-    for c in chars:
+    for c in s:
         if stack and stack[-1] == c:
             stack.pop()
         else:
@@ -16,7 +16,53 @@ def remove_dublicates(chars: str):
     return ''.join(stack)
 
 
-print(remove_dublicates("abbaca"))
+print(remove_duplicates_nabours("abbaca"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def remove_dublicates(chars: str):
+#     stack = []
+#     for c in chars:
+#         if stack and stack[-1] == c:
+#             stack.pop()
+#         else:
+#             stack.append(c)
+
+#     return ''.join(stack)
+
+
+# print(remove_dublicates("abbaca"))
 
 
 
