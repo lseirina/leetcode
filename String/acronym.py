e758674c09106da809ad99edf7a3f06653d1ad36
root <lseirina87@gmail.com>
2024-07-22 12:53:38 +0300

    Added compress_string

--- String/acronym.py
+++ String/acronym.py
@@ -1,110 +1,45 @@
 # print(isAcronym(["alice","bob","charlie"], "abc")) True
 
-def check_acronym(words, acronym):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def my_decorator(func):
+    def wrapper(*args, **kwargs):
+        resultat = func(*args, **kwargs)
+        resultat = False
+        return resultat
+    return wrapper
+
+
+def confirm_acronym(words, acronym):
     res = []
     for word in words:
         res.append(word[0])
-
     if ''.join(res) == acronym:
         return True
     return False
 
 
-print(check_acronym(["alice","bob","charlie"], "abc"))
+print(confirm_acronym(["alice","bob","charlie"], "abc"))
 
 
+def test_confirm_acronym():
+    assert confirm_acronym(["alice","bob","charlie"], "abc") == True
+    print('Tests passed')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def my_decorator(func):
-#     def wrapper(*args, **kwargs):
-#         resultat = func(*args, **kwargs)
-#         resultat = False
-#         return resultat
-#     return wrapper
-
-
-# def confirm_acronym(words, acronym):
-#     res = []
-#     for word in words:
-#         res.append(word[0])
-#     if ''.join(res) == acronym:
-#         return True
-#     return False
-
-
-# print(confirm_acronym(["alice","bob","charlie"], "abc"))
-
-
-# def test_confirm_acronym():
-#     assert confirm_acronym(["alice","bob","charlie"], "abc") == True
-#     print('Tests passed')
-
-# test_confirm_acronym()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def isAcronym(words, s):
-#         res = []
-#         for word in words:
-#             res.append(word[0])
-
-#         if ''.join(res) == s:
-#             return True
-
-#         return False
-
-# print(isAcronym(["alice","bob","charlie"], "abc"))
+test_confirm_acronym()
--- String/compress_string.py
+++ String/compress_string.py
@@ -1,22 +1,66 @@
 # my_str = 'aabcccccaaa' -> 'a2b1c5a3'
 
+
 def compress_string(s):
+    compress_str = ''
     count = 1
-    compress_string = ''
     new_char = s[0]
     for i in range(1, len(s)):
         if s[i] == new_char:
             count += 1
         else:
-            compress_string += new_char + str(count)
+            compress_str += new_char + str(count)
             count = 1
             new_char = s[i]
 
-    compress_string += new_char + str(count)
-
-    if len(compress_string) < len(s):
-        return compress_string
-    return s
+    compress_str += new_char + str(count)
+    return compress_str
 
 
 print(compress_string('aabcccccaaa'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def compress_string(s):
+#     count = 1
+#     compress_string = ''
+#     new_char = s[0]
+#     for i in range(1, len(s)):
+#         if s[i] == new_char:
+#             count += 1
+#         else:
+#             compress_string += new_char + str(count)
+#             count = 1
+#             new_char = s[i]
+
+#     compress_string += new_char + str(count)
+
+#     if len(compress_string) < len(s):
+#         return compress_string
+#     return s
+
+
+# print(compress_string('aabcccccaaa'))
