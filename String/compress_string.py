f124af92a004deff5319617640f177a1d2312057
root <lseirina87@gmail.com>
2024-07-22 13:38:04 +0300

    Added number_of_str

--- String/compress_string.py
+++ String/compress_string.py
@@ -2,65 +2,22 @@
 
 
 def compress_string(s):
-    compress_str = ''
     count = 1
+    compress_string = ''
     new_char = s[0]
     for i in range(1, len(s)):
         if s[i] == new_char:
             count += 1
         else:
-            compress_str += new_char + str(count)
+            compress_string += new_char + str(count)
             count = 1
             new_char = s[i]
 
-    compress_str += new_char + str(count)
-    return compress_str
+    compress_string += new_char + str(count)
+
+    if len(compress_string) < len(s):
+        return compress_string
+    return s
 
 
 print(compress_string('aabcccccaaa'))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def compress_string(s):
-#     count = 1
-#     compress_string = ''
-#     new_char = s[0]
-#     for i in range(1, len(s)):
-#         if s[i] == new_char:
-#             count += 1
-#         else:
-#             compress_string += new_char + str(count)
-#             count = 1
-#             new_char = s[i]
-
-#     compress_string += new_char + str(count)
-
-#     if len(compress_string) < len(s):
-#         return compress_string
-#     return s
-
-
-# print(compress_string('aabcccccaaa'))
--- String/number_of_string.py
+++ String/number_of_string.py
@@ -8,9 +8,41 @@ Explanation:
 - "d" does not appear as a substring in "abc".
 3 of the strings in patterns appear as a substring in word.
 """
-def count_number_of_strings(patterns, word)
+
+
+def count_number_str(patterns, word):
     count = 0
     for pattern in patterns:
         if pattern in word:
             count += 1
     return count
+
+
+print(count_number_str(["a","abc","bc","d"], "abc"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def count_number_of_strings(patterns, word)
+#     count = 0
+#     for pattern in patterns:
+#         if pattern in word:
+#             count += 1
+#     return count
