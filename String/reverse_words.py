7ca40bb50d80a7cd3900f037db9c2a9ba8717586
root <lseirina87@gmail.com>
2024-07-22 14:24:16 +0300

    Added reverse_wordss

--- String/reverse_words.py
+++ String/reverse_words.py
@@ -3,14 +3,14 @@
 
 
 def reverse_words(sentence):
-    sentence = sentence.split(' ')
-    i = 0
-    for i in range(len(sentence)):
-        sentence[i] = sentence[i][::-1]
-        i += 1
+    res = []
+    s_sentence = sentence.split(' ')
+    for word in s_sentence:
+        res.append(word[::-1])
+
+    return ' '.join(res)
 
 
-    return ' '.join(sentence)
 
 print(reverse_words("Let's take LeetCode contest"))
 
@@ -34,12 +34,46 @@ print(reverse_words("Let's take LeetCode contest"))
 
 
 
-def reverseWords(s):
-        s = s.split()
-        res = []
-        for c in s:
-            res.append(c[:: -1])
 
-        return " ".join(res)
 
-print(reverseWords("Let's take LeetCode contest"))
+# def reverse_words(sentence):
+#     sentence = sentence.split(' ')
+#     i = 0
+#     for i in range(len(sentence)):
+#         sentence[i] = sentence[i][::-1]
+#         i += 1
+
+
+#     return ' '.join(sentence)
+
+# print(reverse_words("Let's take LeetCode contest"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def reverseWords(s):
+#         s = s.split()
+#         res = []
+#         for c in s:
+#             res.append(c[:: -1])
+
+#         return " ".join(res)
+
+# print(reverseWords("Let's take LeetCode contest"))
