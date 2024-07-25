acd1c6162b38bcf6fbe86bb9a1f34c881c461e5c
root <lseirina87@gmail.com>
2024-07-25 18:18:04 +0300

    Added inorder_traversal

---
+++ Tree/inorder_traversal.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if root is None:
+            return []
+        return self.inorderTraversal(root.left) + [root.val] +
+        + self.inorderTraversal(root.right)
+
+        return self.inorderTraversal(root)
