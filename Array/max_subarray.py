2e0b5e14bc336dc5408313356554b33873538eee
root <lseirina87@gmail.com>
2024-07-27 21:47:21 +0300

    Added max_subarray + comments

--- Array/max_subarray.py
+++ Array/max_subarray.py
@@ -2,6 +2,8 @@ def max_subarray(nums):
     max_sum = current_sum = nums[0]
 
     for num in nums:
+        # This step decides whether to add the current element to the existing subarray
+        # or start a new subarray with the current element.
         current_sum = max(num, current_sum + num)
         max_sum = max(max_sum, current_sum)
 
