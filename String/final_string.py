1e9426d207367a75c83e11c4da33b3229ceb15fb
root <lseirina87@gmail.com>
2024-07-28 18:18:16 +0300

    Added final_string

---
+++ String/final_string.py
@@ -0,0 +1,20 @@
+"""
+Your laptop keyboard is faulty,
+and whenever you type a character 'i' on it,
+it reverses the string that you have written.
+Typing other characters works as expected.
+Input: s = "string"
+Output: "rtsng"
+"""
+def final_string(s):
+    result = []
+
+    for char in s:
+        if char == 'i':
+            result.reverse()
+        else:
+            result.append(char)
+
+    return ''.join(result)
+
+print(final_string("string"))
