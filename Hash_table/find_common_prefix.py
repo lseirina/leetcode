7adf79761331a5c97c18a7c63c4d1d6ca6b59e6c
root <lseirina87@gmail.com>
2024-08-05 21:38:26 +0300

    Added find common prefix

---
+++ Hash_table/find_common_prefix.py
@@ -0,0 +1,25 @@
+"""
+Input: A = [1,3,2,4], B = [3,1,2,4]
+Output: [0,2,3,4]
+Explanation: At i = 0: no number is common, so C[0] = 0.
+At i = 1: 1 and 3 are common in A and B, so C[1] = 2.
+At i = 2: 1, 2, and 3 are common in A and B, so C[2] = 3.
+At i = 3: 1, 2, 3, and 4 are common in A and B, so C[3] = 4.
+"""
+
+def find_common_prefix(A, B):
+    set_A = set()
+    set_B = set()
+    n = len(A)
+    C = [0]*n
+
+    for i in range(n):
+        set_A.add(A[i])
+        set_B.add(B[i])
+
+        C[i] = len(set_A.intersection(set_B))
+
+    return C
+
+
+print(find_common_prefix([1,3,2,4], [3,1,2,4]))
