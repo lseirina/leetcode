d7ba70672834fe917157ec4cd5854fc2409efc40
root <lseirina87@gmail.com>
2024-08-06 22:24:58 +0300

    Added biggest_single_number

---
+++ Hash_table/minimum_pushes.py
@@ -0,0 +1,15 @@
+"""
+Input: word = "abcde"
+Output: 5
+Explanation: The remapped keypad given in the image provides the minimum cost.
+"a" -> one push on key 2
+"b" -> one push on key 3
+"c" -> one push on key 4
+"d" -> one push on key 5
+"e" -> one push on key 6
+Total cost is 1 + 1 + 1 + 1 + 1 = 5.
+"""
+
+
+def find_minimum_pushes(word):
+   
--- Tree/inorder_traversal.py
+++ Tree/inorder_traversal.py
@@ -4,6 +4,7 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[int]
         """
+
         if root is None:
             return []
         return self.inorderTraversal(root.left) + [root.val]
