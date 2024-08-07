863b089b486e15dd56cf98912b44b8663c03379c
root <lseirina87@gmail.com>
2024-08-07 20:30:02 +0300

    Added complete_day hashmap

--- Hash_table/complete_day.py
+++ Hash_table/complete_day.py
@@ -10,14 +10,42 @@ The pairs of indices that form a complete day are (0, 1) and (3, 4).
 
 """
 
-def count_complete_day_pairs(hours):
+def count_complete_day(hours):
     count = 0
-    for i in range(len(hours)):
-        for j in range(i+1, len(hours)):
-            if (hours[i] + hours[j]) % 24 == 0:
-                count += 1
+    count_remainder = {}
+
+    for hour in hours:
+        remainder = hour % 24
+        complement = (24 - remainder) % 24
+
+        if complement in count_remainder:
+            count += count_remainder[complement]
+
+        if remainder in count_remainder:
+            count_remainder[remainder] += 1
+        else:
+            count_remainder[remainder] = 1
 
     return count
 
 
-print(count_complete_day_pairs([72,48,24,3]))
+print(count_complete_day([12,12,30,24,24]))
+
+
+
+
+
+
+
+
+# def count_complete_day_pairs(hours):
+#     count = 0
+#     for i in range(len(hours)):
+#         for j in range(i+1, len(hours)):
+#             if (hours[i] + hours[j]) % 24 == 0:
+#                 count += 1
+
+#     return count
+
+
+# print(count_complete_day_pairs([72,48,24,3]))
