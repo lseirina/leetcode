ff4b005262d430450a11f6f1b7831caf41c9f807
root <lseirina87@gmail.com>
2024-08-10 22:05:05 +0300

    Added product of array

---
+++ Array/product_of_array.py
@@ -0,0 +1,19 @@
+"""
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+"""
+import math
+
+def product_of_array(nums):
+    res = [0] * len(nums)
+    for i in range(len(nums)):
+        if i == 0:
+            res[i] = math.prod(nums[i+1:])
+        elif i == len(nums) - 1:
+            res[i] = math.prod(nums[:i])
+        else:
+            res[i] = math.prod(nums[:i]) * math.prod(nums[i+1:])
+
+    return res
+
+print(product_of_array([1,2,3,4]))
