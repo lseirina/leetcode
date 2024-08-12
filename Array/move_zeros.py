8ec9aa93f5e699f2166e01462b49b14c5af59563
root <lseirina87@gmail.com>
2024-08-12 21:32:38 +0300

    Added move_zeros

---
+++ Array/move_zeros.py
@@ -0,0 +1,21 @@
+"""
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+"""
+
+
+def move_zeros(nums):
+    non_zero_index = 0
+
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[non_zero_index] = nums[i]
+            non_zero_index += 1
+
+    for i in range(non_zero_index, len(nums)):
+        nums[i] = 0
+
+    return nums
+
+
+print(move_zeros([0,1,0,3,12]))
