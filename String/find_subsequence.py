2b4ff212205f20923a9ce6406c3a3ad596db9741
root <lseirina87@gmail.com>
2024-08-13 17:41:12 +0300

    Added find subsequence

---
+++ String/find_subsequence.py
@@ -0,0 +1,20 @@
+"""
+Input: s = "ab", t = "baab"
+Output: true
+"""
+
+
+def find_subsequence(s, t):
+    subsequence_index = 0
+    for i in range(len(t)):
+        if subsequence_index >= len(s):  # Check if we've matched the entire subsequence
+            break
+        if t[i] == s[subsequence_index]:
+            subsequence_index += 1
+
+    if len(s) == subsequence_index:
+        return True
+    return False
+
+
+print(find_subsequence('ab', 'baab'))
