0a41692f5b6b0b8dd09f9f53e507af1b278fbb48
root <lseirina87@gmail.com>
2024-08-14 22:36:22 +0300

    Added max_operation

--- Array/max_operations.py
+++ Array/max_operations.py
@@ -11,22 +11,54 @@ from collections import Counter
 
 
 def find_max_operation(nums, k):
-    freq = Counter(nums)
     count = 0
-    for num in nums:
-        complement = k - num
-        if freq[num] > 0 and freq[complement] > 0:
-            if num == complement:
-                if freq[num] >= 2:
-                    count += 1
-                    freq[num] -= 2
-
-            else:
-                count += 1
-                freq[num] -= 1
-                freq[complement] -= 1
+    left = 0
+    right = len(nums) - 1
+    nums.sort()
+    while left < right:
+        current_sum = nums[left] + nums[right]
+        if current_sum == k:
+            count += 1
+            left += 1
+            right -= 1
+        elif current_sum < k:
+            left += 1
+        else:
+            right -= 1
 
     return count
 
 
 print(find_max_operation([2,2,2,3,1,1,4,1], 4))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # freq = Counter(nums)
+    # count = 0
+    # for num in nums:
+    #     complement = k - num
+    #     if freq[num] > 0 and freq[complement] > 0:
+    #         if num == complement:
+    #             if freq[num] >= 2:
+    #                 count += 1
+    #                 freq[num] -= 2
+
+    #         else:
+    #             count += 1
+    #             freq[num] -= 1
+    #             freq[complement] -= 1
+
+    # return count
