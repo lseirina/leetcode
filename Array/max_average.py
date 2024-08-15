de646bab22e80b756c6d5dc39e563938c73811f5
root <lseirina87@gmail.com>
2024-08-15 22:17:42 +0300

    Added max_average

--- Array/max_average.py
+++ Array/max_average.py
@@ -5,21 +5,19 @@ Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
 """
 
 def find_max_average(nums, k):
-    window_sum = 0
-    start = 0
-
-    for i in range(k):
-        window_sum += nums[i]
+    window_sum = sum(nums[:k])
+    # for i in range(k):
+    #     window_sum += nums[i]
 
     max_avg = window_sum / k
 
-    for end in range(k, len(nums)):
-        window_sum += nums[end] - nums[start]
+    for i in range(k, len(nums)):
+        window_sum += nums[i] - nums[i-k]
         current_avg = window_sum / k
         max_avg = max(max_avg, current_avg)
-        start += 1
-
+        
     return max_avg
+
 
 
 print(find_max_average([1,12,-5,-6,50,3], 4))
