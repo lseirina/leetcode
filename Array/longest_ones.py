3990ca959f6f83b475a2a861b460b5a0e64be78c
root <lseirina87@gmail.com>
2024-08-17 20:32:18 +0300

    Added longest ones

--- Array/longest_ones.py
+++ Array/longest_ones.py
@@ -10,11 +10,12 @@ def find_longest_ones(nums, k):
     zero_count = 0
 
     for right in range(len(nums)):
-        if right == 0:
+        if nums[right] == 0:
             zero_count += 1
 
         while zero_count > k:
-            zero_count -= 1
+            if nums[left] == 0:
+                zero_count -= 1
             left += 1
 
         max_len = max(max_len, right - left + 1)
@@ -23,4 +24,3 @@ def find_longest_ones(nums, k):
 
 
 print(find_longest_ones([0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], 3))
-
