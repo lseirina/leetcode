d68bdebc8da633114905c101fa5e1218744b5941
root <lseirina87@gmail.com>
2024-08-18 22:24:13 +0300

    Added remove stars

--- Hash_table/equal_pairs.py
+++ Hash_table/equal_pairs.py
@@ -8,19 +8,13 @@ Explanation: There is 1 equal row and column pair:
 def find_equal_pairs(grid):
     count = 0
     columns = list(zip(*grid))
-    # for row in grid:
-    #     for col in columns:
-    #         if tuple(row) == col:
-    #             count += 1
-
     for row in grid:
-        if tuple(row) in columns:
-            count += 1
+        for col in columns:
+            if tuple(row) == col:
+                count += 1
+
 
     return count
 
 
-
-
-
-print(find_equal_pairs([[3,2,1],[1,7,6],[2,7,7]]))
+print(find_equal_pairs([[3,1,2,2],[1,4,4,4],[2,4,2,2],[2,5,2,2]]))
---
+++ stack/remove_stars.py
@@ -0,0 +1,21 @@
+"""
+Input: s = "leet**cod*e"
+Output: "lecoe"
+Explanation: Performing the removals from left to right:
+- The closest character to the 1st star is 't' in "leet**cod*e". s becomes "lee*cod*e".
+- The closest character to the 2nd star is 'e' in "lee*cod*e". s becomes "lecod*e".
+- The closest character to the 3rd star is 'd' in "lecod*e". s becomes "lecoe".
+There are no more stars, so we return "lecoe".
+"""
+def remove_stars(s):
+    res = []
+    for c in s:
+        if res and c == '*':
+            res.pop()
+        else:
+            res.append(c)
+
+    return ''.join(res)
+
+
+print(remove_stars("leet**cod*e"))
