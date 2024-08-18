ef8957ae83ac3901dde8ec11b651c94404ddb9ae
root <lseirina87@gmail.com>
2024-08-18 20:52:49 +0300

    Added equal_pairs

---
+++ Hash_table/equal_pairs.py
@@ -0,0 +1,21 @@
+"""
+Input: grid = [[3,2,1],[1,7,6],[2,7,7]]
+Output: 1
+Explanation: There is 1 equal row and column pair:
+- (Row 2, Column 1): [2,7,7]
+"""
+
+def find_equal_pairs(grid):
+    count = 0
+    columns = list(zip(*grid))
+    for row in grid:
+        for col in columns:
+            if tuple(row) == col:
+                count += 1
+
+    return count
+
+
+
+
+print(find_equal_pairs([[3,2,1],[1,7,6],[2,7,7]]))
