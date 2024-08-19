a75bf412accdaff71f7b207db373fdf8955e79ff
root <lseirina87@gmail.com>
2024-08-19 22:16:48 +0300

    Added merge_sorted_array

--- Array/merge_sorted_array.py
+++ Array/merge_sorted_array.py
@@ -4,8 +4,14 @@ Output: [1,2,2,3,5,6]
 Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
 The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
 """
+
+
 def merge_sorted_array(nums1, m, nums2, n):
-    return sorted(nums1[:m] + nums2[:n])
+    # return sorted(nums1[:m] + nums2[:n])
+    for j in range(n):
+        nums1[m+j] = nums2[j]
+    nums1.sort()
+    return nums1
 
 
 print(merge_sorted_array([1,2,3,0,0,0], 3, [2,5,6], 3))
