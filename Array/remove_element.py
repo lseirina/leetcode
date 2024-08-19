c2b13943c7b567b48b65532aed4a8f4d11c4ab25
root <lseirina87@gmail.com>
2024-08-19 22:41:23 +0300

    Added remove_element

---
+++ Array/remove_element.py
@@ -0,0 +1,16 @@
+"""
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+"""
+def remove_element(nums, val):
+
+    for i in range(len(nums) - 1, -1, -1):
+        if nums[i] == val:
+            nums.pop(i)
+
+    return len(nums)
+
+
+print(remove_element([0,1,2,2,3,0,4,2], 2))
