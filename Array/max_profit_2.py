3bae1cb73c96d665c10e5ee52ba65f5f8df07f46
root <lseirina87@gmail.com>
2024-08-20 22:48:41 +0300

    Added find_max_profit

---
+++ Array/max_profit_2.py
@@ -0,0 +1,18 @@
+"""
+Input: prices = [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Total profit is 4 + 3 = 7.
+"""
+
+def find_max_profit(prices):
+    max_profit = 0
+    for i in range(1, len(prices)):
+        if prices[i] > prices[i-1]:
+            max_profit += prices[i] - prices[i-1]
+
+    return max_profit
+
+
+print(find_max_profit([7, 1, 5, 3, 6, 4]))
