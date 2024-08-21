b85a5b715101d0092a04a116a876b6a55dc01957
root <lseirina87@gmail.com>
2024-08-21 14:58:57 +0300

    Added can_jump

---
+++ Array/can_jump.py
@@ -0,0 +1,16 @@
+"""
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+[3,2,1,1,0,1]
+[2,0,0]
+"""
+def can_jump(nums):
+    last_position = len(nums) - 1
+    for i in range(len(nums) - 2, -1, -1):
+        if (nums[i] + 1) >= last_position:
+            last_position = i
+    return last_position == 0
+
+
+print(can_jump([2,3,1,1,4]))
