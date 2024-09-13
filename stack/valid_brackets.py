e4296599e4ed784f6949c29d81cd1eddcb35c4f3
root <lseirina87@gmail.com>
2024-09-13 11:51:35 +0300

    Added check vakid brackets

--- stack/valid_brackets.py
+++ stack/valid_brackets.py
@@ -1,15 +1,23 @@
 def check_valid_brackets(brackets):
-    dict_brackets = {'(': ')', '[': ']', '{': '}'}
+    dict_brackets = {')': '(', '}': '{', ']': '['}
     stack = []
     for bracket in brackets:
-        if stack and bracket in dict_brackets:
-
+        if bracket in dict_brackets:
+            top_element = stack.pop() if stack else '#'
+            if dict_brackets[bracket] != top_element:
+                return False
         else:
             stack.append(bracket)
 
+    return not stack
 
+
+print(check_valid_brackets('[{}]'))
 
 
 def test():
     assert check_valid_brackets('({})') is True
     assert check_valid_brackets('[(})') is False
+
+
+test()
