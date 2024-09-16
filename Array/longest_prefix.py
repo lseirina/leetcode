516604cee201ed645f9c62654470261391e5d75d
root <lseirina87@gmail.com>
2024-09-16 20:41:30 +0300

    Added longest_prefix

--- Array/longest_prefix.py
+++ Array/longest_prefix.py
@@ -5,23 +5,16 @@ Output: "fl"
 from collections import defaultdict
 
 def longestCommonPrefix(strs):
+    if not strs:
+        return ''
+    min_str = min(strs, key=len)
 
-    min_len = min(strs, key=lambda x: len(x))
+    for i in range(len(min_str)):
+        for s in strs:
+            if s[i] != min_str[i]:
+                return min_str[:i]
 
-    res = []
-    ans = []
-    for i in range(len(strs)):
-        if min_len[0] != strs[i][0]:
-            return ''
-        for j in range(len(min_len)):
-            if min_len[j] == strs[i][j]:
-                res.append(min_len[j])
-            if res and j == len(min_len)-1:
-                ans.append(res)
-                res = []
-
-    min_ans = min(ans)
-    return ''.join(min_ans)
+    return min_str
 
 
 print(longestCommonPrefix(["flower","flow","flight"]))
