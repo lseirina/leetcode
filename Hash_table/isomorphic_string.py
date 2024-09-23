9de25a0e9ad4b31931c1a37919d4b93f7c7bfdc1
root <lseirina87@gmail.com>
2024-09-23 12:26:32 +0300

    Added is_isomorphic

--- Hash_table/isomorphic_string.py
+++ Hash_table/isomorphic_string.py
@@ -5,24 +5,17 @@ Output: true
 from collections import Counter
 
 def is_isomorphic(s, t):
-    my_dict = {}
-    i = 0
-    j = 0
-    res = []
-    for c in s:
-        if t[i] in my_dict.values():
+    if len(s) != len(t):
+        return False
+    my_dict1 = {}
+    my_dict2 = {}
+    for c1, c2 in zip(s, t):
+        if (c1 in my_dict1 and my_dict1[c1] != c2) or (c2 in my_dict2 and my_dict2[c2] != c1):
             return False
-        my_dict[c] = t[i]
-        i += 1
+        my_dict1[c1] = c2
+        my_dict2[c2] = c1
+    return True
 
-    for c in s:
-        if my_dict[c] == t[j]:
-            j += 1
-            res.append(c)
-
-    if len(res) == len(t):
-        return True
-    return False
 
 
 print(is_isomorphic("paper", "title"))
