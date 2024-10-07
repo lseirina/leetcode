799404365fd988d55d064901a05eead267bd6db8
root <lseirina87@gmail.com>
2024-10-07 11:38:23 +0300

    Added happy_number

--- Hash_table/happy_number.py
+++ Hash_table/happy_number.py
@@ -10,19 +10,16 @@ Explanation:
 
 
 def find_happy_number(n):
-
-    def get_next(number):
-        total_sum = 0
-        while number > 0:
-            digit = number % 10
-            total_sum += digit ** 2
-            number //= 10
-        return total_sum
-
     seen = set()
     while n != 1 and n not in seen:
         seen.add(n)
-        n = get_next(n)
+        total_sum = 0
+        while n > 0:
+            digit = n % 10
+            total_sum += digit ** 2
+            n //= 10
+
+        n = total_sum
 
     return n == 1
 
