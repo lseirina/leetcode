a258c70cc0c371c53a37425730af504fceb2b821
root <lseirina87@gmail.com>
2025-08-05 12:07:26 +0300

    Added lambda max

--- lambda/base_lambda.py
+++ lambda/base_lambda.py
@@ -1,12 +1,29 @@
 
+a = {
+    1: 5,
+    2: 4,
+    3: 2,
+}
+print(max(a, key=lambda x: a[x]))
 
-def add_number(x, y):
-    return x + y
 
 
-add_lambda = lambda x, y: x + y
 
-print(add_number(3,5))
-print(add_lambda(3,5))
+
+
+
+
+
+
+
+
+
+
+# def add_number(x, y):
+#     return x + y
+# add_lambda = lambda x, y: x + y
+
+# print(add_number(3,5))
+# print(add_lambda(3,5))
 
 
---
+++ lambda/filter_lambda/filter_zip.py
@@ -0,0 +1,32 @@
+# a = [i for i in range(5)]
+
+# b = filter(lambda x: x % 2 == 0, a)
+# for c in b:
+#     print(c)
+
+a = [1, 2, 3]
+b = [4, 5, 6, 7, 8, ]
+c = [9, 10, 11]
+for i in zip(a, b, c):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# nums = [i for i in range(1, 6)]
+
+# even_numbers = list(filter(lambda x: x % 2 == 0, nums))
+
+# print(even_numbers)
