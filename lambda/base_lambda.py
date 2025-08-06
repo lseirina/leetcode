e049c028424843855cc1618850e5c23a22b347c0
root <lseirina87@gmail.com>
2025-08-06 11:28:14 +0300

    Added combining two tables

--- lambda/base_lambda.py
+++ lambda/base_lambda.py
@@ -6,24 +6,11 @@ a = {
 }
 print(max(a, key=lambda x: a[x]))
 
+def add_number(x, y):
+    return x + y
+add_lambda = lambda x, y: x + y
 
-
-
-
-
-
-
-
-
-
-
-
-
-# def add_number(x, y):
-#     return x + y
-# add_lambda = lambda x, y: x + y
-
-# print(add_number(3,5))
-# print(add_lambda(3,5))
+print(add_number(3,5))
+print(add_lambda(3,5))
 
 
