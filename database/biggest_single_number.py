ca4b05c7df44e1bca7b7b64beca616c4be1f03de
root <lseirina87@gmail.com>
2025-08-06 14:36:09 +0300

    Added biggest singlr number

--- database/biggest_single_number.py
+++ database/biggest_single_number.py
@@ -1,2 +1,11 @@
 SELECT MAX(num) as num
 FROM (SELECT num FROM MyNumbers GROUP BY num HAVING COUNT(num) = 1) AS a
+
+SELECT MAX(num) AS num
+FROM MyNumbers
+WHERE num IN (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+);
