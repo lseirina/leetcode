cac7d2513ccb2f5f78bfc024fe2689322afe5c40
root <lseirina87@gmail.com>
2025-08-08 15:54:17 +0300

    Added min_prefix

--- Array/longest_prefix.py
+++ Array/longest_prefix.py
@@ -2,13 +2,11 @@
 Input: strs = ["flower","flow","flight"]
 Output: "fl"
 """
-from collections import defaultdict
 
 def longestCommonPrefix(strs):
     if not strs:
         return ''
     min_str = min(strs, key=len)
-
     for i in range(len(min_str)):
         for s in strs:
             if s[i] != min_str[i]:
@@ -17,4 +15,4 @@ def longestCommonPrefix(strs):
     return min_str
 
 
-print(longestCommonPrefix(["flower","flow","flight"]))
+print(longestCommonPrefix(["flower","flos","flight"]))
