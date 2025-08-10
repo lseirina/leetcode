83664bebc7a41d92917cfbde3fefe5274551eba0
root <lseirina87@gmail.com>
2025-08-10 11:39:28 +0300

    Added majority element

--- Array/majority_element.py
+++ Array/majority_element.py
@@ -8,7 +8,7 @@ def find_majority_element(nums):
     count = Counter(nums)
     for k, v in count.items():
         if v == max(count.values()):
-            return k
+            return count
 
 
-print(find_majority_element([2,2,1,1,1,2,2]))
+print(find_majority_element([2, 2, 1, 1, 1, 2, 2]))
