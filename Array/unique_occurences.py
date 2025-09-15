cf99c3a8e6b98058dc741c42eaccb0c13f08a3bf
root <lseirina87@gmail.com>
2025-09-15 16:46:33 +0300

    Added unique_occurence_dec

--- Array/unique_occurences.py
+++ Array/unique_occurences.py
@@ -4,8 +4,18 @@ Output: true
 Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1.
 No two values have the same number of occurrences.
 """
+
 from collections import Counter
 
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        if result is True:
+            print("Hello")
+        return result
+    return wrapper
+
+@decorator
 def check_unique_occurences(nums):
     count_nums = Counter(nums)
     if len(set(count_nums.values())) == len(set(nums)):
@@ -15,4 +25,4 @@ def check_unique_occurences(nums):
 
 
 
-print(check_unique_occurences([1,2,2,1,1,3,2]))
+print(check_unique_occurences([1,2,2,1,3,2]))
