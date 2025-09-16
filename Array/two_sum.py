5b2af732893e96d48345364b6aa7ce22c1bb0771
root <lseirina87@gmail.com>
2025-09-16 12:36:23 +0300

    Added twosum dec

--- Array/two_sum.py
+++ Array/two_sum.py
@@ -1,12 +1,22 @@
 # Input: nums = [2,7,11,15], target = 9
 # Output: [0,1]
 # Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-def twoSum(self, nums, target):
+def my_decorator(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        print('Hello, world')
+        return result
+    return wrapper
+      
+@my_decorator  
+def twoSum(nums, target):
     num_map = {}
-    n = len(nums)
-
-    for i in range(n):
-        num = target - nums[i]
-        if num in num_map:
-            return [num_map[num], i]
+    for i in range(len(nums)):
+        if (target - nums[i]) in num_map:
+            return [num_map[target-nums[i]], i]
         num_map[nums[i]] = i
+
+    return num_map
+    
+
+print(twoSum([2,7,11,15], 9))
